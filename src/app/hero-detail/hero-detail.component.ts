b73fec0b8e9dfeb6b50d741cad79ec56e3b88edc
SrMarchetti <marchetti.sr@gmail.com>
2024-09-01 22:44:09 -0300

    Aula 5 completada

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component, Input } from '@angular/core';
-import {NgIf, UpperCasePipe} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+//mport {NgIf, UpperCasePipe} from '@angular/common';
+//import {FormsModule} from '@angular/forms';
 
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 @Component({
   //standalone: true,
@@ -14,6 +17,26 @@ import { Hero } from '../hero';
 
 
 
-export class HeroDetailComponent {
-  @Input() hero?: Hero;
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
